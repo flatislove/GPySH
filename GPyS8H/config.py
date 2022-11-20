687b90bbb4fa63f0ae22656233782d0d369506b4
flatislove <flatislove@gmail.com>
2022-11-20 21:07:23 +0600

    s8

---
+++ GPyS8H/config.py
@@ -0,0 +1,6 @@
+host="127.0.0.1"
+user="flvtisv"
+password="123456"
+db_name="python_s8"
+port="5432"
+
---
+++ GPyS8H/model/database_operation.py
@@ -0,0 +1,129 @@
+import os
+import psycopg2
+from config import host,user,password,db_name
+import employee
+import department
+import project
+
+def get_from_database(text):
+  try:
+    connection=psycopg2.connect(
+      host=host,
+      user=user,
+      password=password,
+      database=db_name
+    )
+    connection.autocommit=True
+    # with connection.cursor() as cursor:
+    #   cursor.execute("SELECT version();")
+    #   print(f"Server version: {cursor.fetchone()}")
+    with connection.cursor() as cursor:
+      cursor.execute(text)
+      return cursor.fetchall()
+    # with connection.cursor() as cursor:
+    #   cursor.execute(
+    #     """INSERT INTO "Group"(groupname,description) VALUES('11f','dangerous group');"""
+    #   )
+    #   print("[INFO] Data was successfully inserted")
+    # with connection.cursor() as cursor:
+    #   cursor.execute(
+    #     """SELECT name FROM "Department" WHERE name='11f';"""
+    #   )
+    #   data=cursor.fetchall()
+    #   for i in data:
+    #     print(i)
+
+  except Exception as ex:
+    print("[INFO] Error while working with Postgres",ex)
+  finally:
+    if connection:
+      connection.close()
+      print("[INFO] Postgres connection closed")
+
+def add_to_database(add):
+  try:
+    connection=psycopg2.connect(
+      host=host,
+      user=user,
+      password=password,
+      database=db_name
+    )
+    connection.autocommit=True
+    with connection.cursor() as cursor:
+        cursor.execute(add)
+        print("[INFO] Data was successfully inserted")
+
+  except Exception as ex:
+    print("[INFO] Error while working with Postgres",ex)
+  finally:
+    if connection:
+      connection.close()
+    #   print("[INFO] Postgres connection closed")
+
+def add_tables():
+    sql=        """
+        CREATE TABLE IF NOT EXISTS "Department"(
+            id serial PRIMARY KEY,
+            name varchar(50) NOT NULL
+            );
+
+        CREATE TABLE IF NOT EXISTS "Employee"(
+            id serial PRIMARY KEY,
+            firstname varchar(50) NOT NULL,
+            lastname varchar(50) NOT NULL,
+            department_id integer NOT NULL,
+            number varchar(15),
+            position varchar(30),
+            FOREIGN KEY (department_id) REFERENCES "Department" (Id)
+            );
+        
+        CREATE TABLE IF NOT EXISTS "Project"(
+            id serial PRIMARY KEY,
+            name varchar(50) NOT NULL,
+            employee_id integer NOT NULL,
+            FOREIGN KEY (employee_id) REFERENCES "Employee" (Id)
+            );"""
+    add_to_database(sql)
+
+def add_employee(employee):
+    print(employee)
+    add=f"""INSERT INTO "Employee"(firstname,lastname,department_id,number,position)
+            VALUES('{employee.firstname}','{employee.lastname}','{int(employee.department)}','{employee.number}','{employee.position}');"""
+    add_to_database(add)
+
+def add_department(department):
+    add=f"""INSERT INTO "Department"(name) VALUES('{department.name}');"""
+    add_to_database(add)
+
+def add_project(project):
+    add=f"""INSERT INTO "Project"(name,employee_id)
+            VALUES('{project.name}','{project.employee_id}');"""
+    add_to_database(add)
+
+def get_all_employee():
+    get=get_from_database("""SELECT id, firstname, lastname, department_id, number,position FROM "Employee";""")
+    employees=[]
+    for i in get:
+        employees.append(employee.Employee(i[0],i[1],i[2],i[3],i[4],i[5]))
+    return employees
+
+def get_all_departments():
+    get=get_from_database("""SELECT id,name FROM "Department";""")
+    departments=[]
+    for i in get:
+        print(i)
+        departments.append(department.Department(i[0],i[1]))
+    return departments
+
+def get_all_projects():
+    get=get_from_database("""SELECT id, name, employee_id FROM "Project";""")
+    projects=[]
+    for i in get:
+        projects.append(project.Project(i[0],i[1],i[2]))
+    return projects
+
+def get_employee_by_department(department):
+    pass
+
+def get_employee_by_project(project):
+    pass
