955ef12d428c434ecee372b56f845412b433c7ba
flatislove <flatislove@gmail.com>
2022-12-09 04:47:15 +0600

    s6_opti

--- GPySA1/task1.py
+++ GPySA1/task1.py
@@ -6,12 +6,6 @@ def sum_digits(number):
     for i in list(number):
         sum+=int(i) if i.isnumeric() else 0
     print(f"Сумма цифр вещественного числа={sum}")
-
-def sumdigits(number):
-    sum=0
-    sum+=[lambda x: x.isnumeric() for x in number]
-    print(sum)
     
 number=input('Введите вещественное число:')
 sum_digits(number)
-sumdigits(number)
